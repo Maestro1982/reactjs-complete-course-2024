e3f3c485b8d3738285a80aab1b7fd77386dfd6b5
Maestro1982 <kris.tote@telenet.be>
2024-10-09 11:15:27 +0200

    modify Counter.jsx in useReducer folder

--- react-demos/src/hooks/useReducer/Counter.jsx
+++ react-demos/src/hooks/useReducer/Counter.jsx
@@ -6,16 +6,23 @@ const Counter = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const [inputValue, setInputValue] = useState(0);
 
-  const incrementByAmount = () =>
+  const incrementByAmount = () => {
     dispatch({ type: 'incrementByAmount', payload: Number(inputValue) });
-  const decrementByAmount = () =>
+    setInputValue(0);
+  };
+
+  const decrementByAmount = () => {
     dispatch({ type: 'decrementByAmount', payload: Number(inputValue) });
+    setInputValue(0);
+  };
 
   return (
     <div>
       <h1>Count: {state.count}</h1>
-      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
-      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+      <button onClick={() => dispatch({ type: 'increment' })}>Add + 1</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>
+        Subtract - 1
+      </button>
 
       <div>
         <input
