87398ffb38fd3e3ff860245174cfeffe04059596
Maestro1982 <kris.tote@telenet.be>
2024-10-09 14:05:58 +0200

    implement project demos: Meals API fetch

--- project-demos/src/App.jsx
+++ project-demos/src/App.jsx
@@ -1,11 +1,13 @@
 //import Counter from './components/Counter';
 //import TodoList from './components/TodoList';
+import Meals from './components/Meals';
 
 const App = () => {
   return (
     <div>
       {/* <Counter /> */}
       {/* <TodoList /> */}
+      <Meals />
     </div>
   );
 };
---
+++ project-demos/src/components/Meals.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import '../styles.css';
+
+const Meals = () => {
+  const [meals, setMeals] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://www.themealdb.com/api/json/v1/1/filter.php?c=Seafood')
+      .then((res) => {
+        //console.log(res.data.meals);
+        setMeals(res.data.meals);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const itemsList = meals.map(({ strMeal, strMealThumb, idMeal }) => {
+    return (
+      <section className='card'>
+        <img src={strMealThumb} alt={strMeal} />
+        <section className='content'>
+          <p>{strMeal}</p>
+          <p>#{idMeal}</p>
+        </section>
+      </section>
+    );
+  });
+
+  return <div className='items-container'>{itemsList}</div>;
+};
+export default Meals;
