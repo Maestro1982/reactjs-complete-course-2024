4b4f6752a74af039ee944a40b56b16c5fc24266f
Maestro1982 <kris.tote@telenet.be>
2024-10-09 13:06:05 +0200

    implement project demos: Counter

---
+++ project-demos/src/App.jsx
@@ -0,0 +1,12 @@
+//import Counter from './components/Counter';
+import TodoList from './components/TodoList';
+
+const App = () => {
+  return (
+    <div>
+      {/* <Counter /> */}
+      <TodoList />
+    </div>
+  );
+};
+export default App;
